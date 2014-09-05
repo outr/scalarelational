50f68f3349172e76dc9746ba85b845cadb747d52
Matt Hicks <matt@outr.com>
2014-09-04 19:51:27 -0500

    Added support to ExistingQuery to utilize NamedArguments.

--- orm/src/main/scala/com/outr/query/orm/existing/ExistingQuery.scala
+++ orm/src/main/scala/com/outr/query/orm/existing/ExistingQuery.scala
@@ -4,6 +4,8 @@ import com.outr.query.Datastore
 import java.sql.ResultSet
 import org.powerscala.reflect._
 
+import scala.annotation.tailrec
+
 /**
  * ExistingQuery allows mapping of an existing query to a result result of type R.
  *
@@ -15,7 +17,11 @@ class ExistingQuery[R](datastore: Datastore, queryString: String)(implicit manif
   private val caseValues = caseClass.caseValues.map(cv => cv.name.toLowerCase -> cv).toMap
 
   def query(args: List[Any]) = {
-    val results = datastore.session.executeQuery(queryString, args)
+    val namedArgs = args.collect {
+      case na: NamedArgument => na
+    }
+    val query = applyNamed(queryString, namedArgs)
+    val results = datastore.session.executeQuery(query, args.filterNot(a => a.isInstanceOf[NamedArgument]))
     new Iterator[R] {
       def hasNext = results.next()
       def next() = result2R(results)
@@ -32,4 +38,15 @@ class ExistingQuery[R](datastore: Datastore, queryString: String)(implicit manif
     }.flatten.toMap
     caseClass.create(args)
   }
+
+  @tailrec
+  private def applyNamed(q: String, args: List[NamedArgument]): String = if (args.isEmpty) {
+    q
+  } else {
+    val arg = args.head
+    val modified = q.replaceAll(s":${arg.key}", arg.value)
+    applyNamed(modified, args.tail)
+  }
 }
+
+case class NamedArgument(key: String, value: String)
