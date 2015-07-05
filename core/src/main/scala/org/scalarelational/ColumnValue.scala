c9e8d22cde2c8d19a9d265784d5c36a68f571d37
Tim Nieradzik <tim.nieradzik@gmx.de>
2015-07-05 16:43:03 +0200

    ColumnValue: Better error reporting

--- core/src/main/scala/org/scalarelational/ColumnValue.scala
+++ core/src/main/scala/org/scalarelational/ColumnValue.scala
@@ -10,10 +10,18 @@ class ColumnValue[T] private(val column: ColumnLike[T],
                              val value: T,
                              val converterOverride: Option[DataType[T]]) extends ExpressionValue[T] {
   def expression: ColumnLike[T] = column
-  def toSQL: Any = converterOverride match {
-    case Some(converter) => converter.toSQLType(column, value)
-    case None => column.converter.toSQLType(column, value)
-  }
+  def toSQL: Any =
+    try {
+      converterOverride match {
+        case Some(converter) => converter.toSQLType(column, value)
+        case None => column.converter.toSQLType(column, value)
+      }
+    } catch {
+      case t: Throwable =>
+        val sourceClass = manifest.runtimeClass
+        val targetClass = value.getClass
+        throw new RuntimeException(s"Invalid conversion from $sourceClass to $targetClass (column = $column, value = $value)")
+    }
 
   override def toString = s"$column: $value"
 }
