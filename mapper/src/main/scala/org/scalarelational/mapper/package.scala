afdc6f5191081263845242a2eb90f94cca70112c
Tim Nieradzik <tim@kognit.io>
2015-07-10 14:26:47 +0200

    Mapper: Ignore all members that are not setters

--- mapper/src/main/scala/org/scalarelational/mapper/package.scala
+++ mapper/src/main/scala/org/scalarelational/mapper/package.scala
@@ -3,10 +3,12 @@ package org.scalarelational
 import org.powerscala.reflect._
 import org.scalarelational.column.property.AutoIncrement
 import org.scalarelational.instruction.{InsertSingle, Instruction, Query}
-import org.scalarelational.model.{Datastore, Column, Table}
+import org.scalarelational.model.{Column, Table}
 import org.scalarelational.result.QueryResult
 
 import scala.util.Try
+import scala.reflect.ClassTag
+import scala.reflect.runtime.currentMirror
 
 /**
  * @author Matt Hicks <matt@outr.com>
@@ -51,26 +53,42 @@ package object mapper {
   }
 
   implicit class MappableTable(table: Table) {
-    private def fieldValues[T <: AnyRef](value: T,
-                                         clazz: EnhancedClass,
-                                         strictMapping: Boolean) = {
-      clazz.fields.flatMap { f =>
-        val column = table.getColumnByField[Any](f.name)
+    def simpleName(fullName: String) =
+      fullName.lastIndexOf('.') match {
+        case -1 => fullName
+        case position => fullName.substring(position + 1)
+      }
+
+    private def fieldValues[T: ClassTag](value: T, strictMapping: Boolean): Seq[ColumnValue[Any]] = {
+      val refl = currentMirror.reflect(value)
+      val members = refl.symbol.asType.typeSignature.members
+      val fields = members.filter(_.asTerm.isVal)
+
+      fields.flatMap { field =>
+        val setter = members
+          .find(member => member.fullName == field.fullName && member.isMethod)
+          .get.asMethod
+
+        val f = simpleName(field.fullName)
+        val column = table.getColumnByField[Any](f)
+
         if (column.isEmpty && strictMapping)
           throw new RuntimeException(s"Field $f has no corresponding column")
-        else
-          Try(column.map(c => c(f[Any](value)))).getOrElse {
+        else {
+          val v = refl.reflectMethod(setter)(value)
+
+          Try(column.map(c => c(v))).getOrElse {
             throw new RuntimeException(s"Field $f incompatible to table column type ${column.get.classType}")
           }
-      }
+        }
+      }.toSeq
     }
 
-    def persist[T <: AnyRef](value: T,
+    def persist[T: ClassTag](value: T,
                              forceInsert: Boolean = false,
                              strictMapping: Boolean = true): Instruction[T] = {
-      val clazz: EnhancedClass = value.getClass
       val primaryColumn = table.primaryKeys.head.asInstanceOf[Column[Any]]
-      val values = fieldValues(value, clazz, strictMapping)
+      val values = fieldValues(value, strictMapping)
       val updates = values.filterNot(_.column == primaryColumn && primaryColumn.has(AutoIncrement))
       values.find(_.column == primaryColumn) match {
         case Some(primaryKey) => {
@@ -86,6 +104,7 @@ package object mapper {
             val update = table.datastore.update(updates: _*) where (primaryColumn === primaryKey.value)
             new InstanceInstruction[T](update, value, table)
           } else {
+            val clazz: EnhancedClass = value.getClass
             val primaryKeyCaseValue = clazz.caseValue(primaryColumn.fieldName).getOrElse(throw new RuntimeException(s"Unable to find case value for ${primaryColumn.name} (field name: ${primaryColumn.fieldName}) in $clazz."))
             new PersistInsertInstruction[T](table.datastore.insert(updates: _*), primaryKeyCaseValue, value)
           }
