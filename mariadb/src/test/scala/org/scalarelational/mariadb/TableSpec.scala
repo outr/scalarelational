688ea50ee6b0b26c420c0d33502d94942b435b1f
Tim Nieradzik <tim@metastack.pl>
2016-06-01 13:45:01 +0200

    MariaDBDatastore: Take `serverTimezone` parameter
    Workaround for http://stackoverflow.com/questions/26515700/mysql-jdbc-driver-5-1-33-time-zone-issue

--- mariadb/src/test/scala/org/scalarelational/mariadb/TableSpec.scala
+++ mariadb/src/test/scala/org/scalarelational/mariadb/TableSpec.scala
@@ -3,13 +3,14 @@ package org.scalarelational.mariadb
 import org.scalarelational.extra.HikariSupport
 import org.scalarelational.{AbstractSpecialTypesDatastore, AbstractTableSpec, AbstractTestCrossReferenceDatastore, AbstractTestDatastore}
 
-
 class TableSpec extends AbstractTableSpec {
   override def testDatastore = TestDatastore
   override def testCrossReference = TestCrossReferenceDatastore
   override def specialTypes = SpecialTypesDatastore
 }
 
-object TestDatastore extends MariaDBDatastore(MariaDBConfig("localhost", "tablespec", "travis", "")) with AbstractTestDatastore with HikariSupport
-object TestCrossReferenceDatastore extends MariaDBDatastore(MariaDBConfig("localhost", "cross_reference", "travis", "")) with AbstractTestCrossReferenceDatastore
-object SpecialTypesDatastore extends MariaDBDatastore(MariaDBConfig("localhost", "special_types", "travis", "")) with AbstractSpecialTypesDatastore
+object TestDatastore extends MariaDBDatastore(MariaDBConfig("localhost", "tablespec", "travis", "", serverTimezone = Some("UTC"))) with AbstractTestDatastore with HikariSupport
+
+object TestCrossReferenceDatastore extends MariaDBDatastore(MariaDBConfig("localhost", "cross_reference", "travis", "", serverTimezone = Some("UTC"))) with AbstractTestCrossReferenceDatastore
+
+object SpecialTypesDatastore extends MariaDBDatastore(MariaDBConfig("localhost", "special_types", "travis", "", serverTimezone = Some("UTC"))) with AbstractSpecialTypesDatastore
