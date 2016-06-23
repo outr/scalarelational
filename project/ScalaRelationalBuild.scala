d44298c3dd7c85b65876c6506d134b89cb1643a6
Matt Hicks <matt@outr.com>
2016-06-23 07:33:17 -0500

    Released 1.3.3

--- project/ScalaRelationalBuild.scala
+++ project/ScalaRelationalBuild.scala
@@ -43,15 +43,6 @@ object ScalaRelationalBuild extends Build {
       Resolver.typesafeRepo("releases")
     ),
     addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full),
-    publishTo <<= version {
-      (v: String) =>
-        val nexus = "https://oss.sonatype.org/"
-        if (v.trim.endsWith("SNAPSHOT")) {
-          Some("snapshots" at nexus + "content/repositories/snapshots")
-        } else {
-          Some("releases" at nexus + "service/local/staging/deploy/maven2")
-        }
-    },
     publishArtifact in Test := false,
     pomExtra := <url>{Details.url}</url>
       <licenses>
@@ -83,7 +74,7 @@ object ScalaRelationalBuild extends Build {
 object Details {
   val organization = "org.scalarelational"
   val name = "scalarelational"
-  val version = "1.3.3-SNAPSHOT"
+  val version = "1.3.3"
   val url = "http://outr.com"
   val licenseType = "Apache 2.0"
   val licenseURL = "http://opensource.org/licenses/Apache-2.0"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,5 @@
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.7.0")
+
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "1.1")
+
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
